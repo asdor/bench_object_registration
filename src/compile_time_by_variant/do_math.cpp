7bfa4cb5619411aefc35f12b012b6d551fa03fc7
asdor <asdor1812@gmail.com>
2025-08-02 17:14:25 +0200

    Implement do_math_by_variant

---
+++ src/compile_time_by_variant/do_math.cpp
@@ -0,0 +1,87 @@
+#include "compile_time_by_variant/do_math.hpp"
+
+#include <string_view>
+#include <variant>
+
+namespace
+{
+  class AdditionOperation
+  {
+  public:
+    int do_operation(int i_x, int i_y) const;
+  };
+
+  class SubtractionOperation
+  {
+  public:
+    int do_operation(int i_x, int i_y) const;
+  };
+
+  class MultiplicationOperation
+  {
+  public:
+    int do_operation(int i_x, int i_y) const;
+  };
+
+  class DivisionOperation
+  {
+  public:
+    int do_operation(int i_x, int i_y) const;
+  };
+
+  int AdditionOperation::do_operation(int i_x, int i_y) const
+  {
+    return i_x + i_y;
+  }
+
+  int SubtractionOperation::do_operation(int i_x, int i_y) const
+  {
+    return i_x - i_y;
+  }
+
+  int MultiplicationOperation::do_operation(int i_x, int i_y) const
+  {
+    return i_x * i_y;
+  }
+
+  int DivisionOperation::do_operation(int i_x, int i_y) const
+  {
+    return i_x / i_y;
+  }
+
+  class OperationManager
+  {
+  private:
+    std::variant<AdditionOperation, SubtractionOperation, MultiplicationOperation, DivisionOperation> d_operation;
+
+  public:
+    void set_operation(std::string_view i_operation)
+    {
+      if (i_operation == "add")
+        d_operation.emplace<AdditionOperation>();
+      else if (i_operation == "sub")
+        d_operation.emplace<SubtractionOperation>();
+      else if (i_operation == "mul")
+        d_operation.emplace<MultiplicationOperation>();
+      else if (i_operation == "div")
+        d_operation.emplace<DivisionOperation>();
+    }
+
+    int do_operation(int i_x, int i_y) const
+    {
+      return std::visit([i_x, i_y](const auto& i_op) { return i_op.do_operation(i_x, i_y); }, d_operation);
+    }
+  };
+}
+
+namespace bench
+{
+
+  int do_math_by_variant(std::string_view i_operation, int i_x, int i_y)
+  {
+    OperationManager manager;
+    manager.set_operation(i_operation);
+
+    return manager.do_operation(i_x, i_y);
+  }
+}
--- tests/unit_tests/test_do_math.cpp
+++ tests/unit_tests/test_do_math.cpp
@@ -1,3 +1,4 @@
+#include <compile_time_by_variant/do_math.hpp>
 #include <naive_by_condition/do_math.hpp>
 
 #include <gtest/gtest.h>
@@ -8,4 +9,12 @@ TEST(A, B)
   EXPECT_EQ(bench::do_math_naive("sub", 10, 2), 8);
   EXPECT_EQ(bench::do_math_naive("mul", 87, 42), 3654);
   EXPECT_EQ(bench::do_math_naive("div", 1444, 19), 76);
+}
+
+TEST(A, C)
+{
+  EXPECT_EQ(bench::do_math_by_variant("add", 5, 8), 13);
+  EXPECT_EQ(bench::do_math_by_variant("sub", 10, 2), 8);
+  EXPECT_EQ(bench::do_math_by_variant("mul", 87, 42), 3654);
+  EXPECT_EQ(bench::do_math_by_variant("div", 1444, 19), 76);
 }
