045fb80a47f61d7f55759f50303c8e96e995517d
asdor <asdor1812@gmail.com>
2025-08-02 16:12:47 +0200

    Add naive implementation

---
+++ include/naive_by_condition/do_math.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+
+namespace bench
+{
+  int do_math_naive(std::string_view i_operation, int i_x, int i_y);
+}
---
+++ src/naive_by_condition/do_math.cpp
@@ -0,0 +1,18 @@
+#include "naive_by_condition/do_math.hpp"
+
+namespace bench
+{
+  int do_math_naive(std::string_view i_operation, int i_x, int i_y)
+  {
+    if (i_operation == "add")
+      return i_x + i_y;
+    else if (i_operation == "sub")
+      return i_x - i_y;
+    else if (i_operation == "mul")
+      return i_x * i_y;
+    else if (i_operation == "div")
+      return i_x / i_y;
+    else
+      return -1;
+  }
+}
